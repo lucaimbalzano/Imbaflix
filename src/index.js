a03d5afbab9cc804ea35aa3eeb2cde675ec24e9c
lucaimbalzano <lucaimbalzano@gmail.com>
2022-04-28 17:04:40 +0200

    + added style in jumbotron.js and eslint for autosaveformatter

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import { GlobalStyle } from './global-style'
 
-render(<App/>, document.getElementById('root'));
+render(
+    <React.Fragment>
+        <GlobalStyle/>
+        <App/>
+    </React.Fragment>, document.getElementById('root'));
+
